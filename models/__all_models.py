0aa8accbf4d550378eb32e58b1f2d923ae321edb
marconys <marconys.moura@gmail.com>
2026-03-30 22:12:34 -0300

    feat: criado o arquivo __all_models.py para facilitar a chamda no script de criar tabelas

---
+++ models/__all_models.py
@@ -0,0 +1,9 @@
+from models.conta_model import AccountModel
+from models.transacoes_model import TransactionModel
+from models.usuario_model import UserModel
+
+__all__= [
+    "UserModel",
+    "AccountModel",
+    "TransactionModel"
+]
