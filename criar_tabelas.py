07b3481a0d582aa8a4d3641063a65fe808d49a53
marconys <marconys.moura@gmail.com>
2026-03-30 22:08:56 -0300

    feat: implementado o script criar_tabelas.py e gerado tabelas no banco de dados

---
+++ criar_tabelas.py
@@ -0,0 +1,24 @@
+import asyncio
+
+from core.configs import settings
+from core.database import engine
+
+from models.__all_models import *
+
+async def create_tables():
+    try:
+        async with engine.begin() as conn:
+            print("Criando tabelas...")
+            await conn.run_sync(settings.DB_BASE_MODEL.metadata.drop_all)
+            await conn.run_sync(settings.DB_BASE_MODEL.metadata.create_all)
+        print("Tabelas criadas com sucesso!")
+    except Exception as e:
+        print(f"Erro ao criar tabelas: {e}")
+    
+    finally:
+        await engine.dispose()
+        
+        
+if __name__ == "__main__":
+    asyncio.run(create_tables())         
+        
